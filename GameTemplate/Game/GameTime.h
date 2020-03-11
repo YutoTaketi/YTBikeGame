6684f7d73f283cc4c6bc3735c9d9b67b19c98c47
Yuto Taketi <kbc18b15@stu.kawahara.ac.jp>
2020-03-11 23:47:48 +0900

    StopWatchクラスを追加

--- GameTemplate/Game/GameTime.h
+++ GameTemplate/Game/GameTime.h
@@ -1,6 +1,7 @@
 /*!
  *@brief	ゲーム時間。
  */
+#include <queue>
 #pragma once
 class GameTime
 {
@@ -24,9 +25,25 @@ public:
 	}
 	void PushFrameDeltaTime(float deltaTime)
 	{
-
+		m_frameDeltaTimeQue.push_back(deltaTime);
+		if (m_frameDeltaTimeQue.size() > 30.0f) {
+			float totalTime = 0.0f;
+			for (auto time : m_frameDeltaTimeQue) {
+				totalTime += time;
+			}
+			//平均値をとる。
+			m_frameDeltaTime = min(1.0f / 30.0f, totalTime / m_frameDeltaTimeQue.size());
+			m_frameDeltaTimeQue.pop_front();
+		}
 	}
 private:
+	std::list<float> m_frameDeltaTimeQue;
+	float		m_frameDeltaTime = 1.0f / 60.0f;		//1フレームの経過時間。
 	
 };
+
+static GameTime& GameTime()
+{
+	return GameTime::GetInstance();
+}
 

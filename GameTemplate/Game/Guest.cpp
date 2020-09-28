ad7a3bcbc6a68c9663842033d032b67392af0176
Yuto Taketi <kbc18b15@stu.kawahara.ac.jp>
2020-09-28 13:32:18 +0900

    BikeIdle.wav,BikeSoukou.wavを追加

--- GameTemplate/Game/Guest.cpp
+++ GameTemplate/Game/Guest.cpp
@@ -4,18 +4,18 @@
 
 Guest::Guest()
 {
-	m_model.Init(L"Assets/modelData/Guest.cmo");
+	m_model.Init(L"Assets/modelData/Guest2.cmo");
 	//静的物理オブジェクトを作成
 	//m_phyStaticObject.CreateMeshObject(m_model, m_position, m_rotation);
 
 	//アニメーションクリップのロード
-	m_animationClip[enAnimationClip_Guest_Idle].Load(L"Assets/animData/Guest_Idle.tka");
-	m_animationClip[enAnimationClip_Guest_Kobusi].Load(L"Assets/animData/Guest_Kobusi.tka");
+	//m_animationClip[enAnimationClip_Guest_Idle].Load(L"Assets/animData/Guest_Idle.tka");
+	//m_animationClip[enAnimationClip_Guest_Kobusi].Load(L"Assets/animData/Guest_Kobusi.tka");
 	//ループフラグの設定
-	m_animationClip[enAnimationClip_Guest_Idle].SetLoopFlag(true);
-	m_animationClip[enAnimationClip_Guest_Kobusi].SetLoopFlag(true);
+	//m_animationClip[enAnimationClip_Guest_Idle].SetLoopFlag(true);
+	//m_animationClip[enAnimationClip_Guest_Kobusi].SetLoopFlag(true);
 	//初期化
-	m_animation.Init(m_model, m_animationClip, enAnimationClip_num);
+	//m_animation.Init(m_model, m_animationClip, enAnimationClip_num);
 	//m_animation.Play(enAnimationClip_Guest_Idle, 0.2f);
 
 	m_model.SetShadowReciever(true);
@@ -27,8 +27,8 @@ Guest::~Guest()
 
 void Guest::Update()
 {
-	m_animation.Play(enAnimationClip_Guest_Kobusi, 0.2f);
-	m_animation.Update(1.0f / 30.0f);
+	//m_animation.Play(enAnimationClip_Guest_Kobusi, 0.2f);
+	//m_animation.Update(1.0f / 30.0f);
 	m_model.UpdateWorldMatrix(m_position, m_rotation, m_scale);
 }
 
